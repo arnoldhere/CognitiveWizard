44224e2295fb7e4bd183f85aa8f6b7d076c1504a
arnoldhere <official.arnold.mac.2004@gmail.com>
2026-05-07 17:18:22 +0530

    Dependecies

--- server/utils/preprocess/chunker.py
+++ server/utils/preprocess/chunker.py
@@ -3,6 +3,6 @@ from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 
 def chunker(docs):
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=800, chunk_overlap=100, separators=["\n\n", "\n", ".", " ", ""]
+        chunk_size=800, chunk_overlap=200, separators=["\n\n", "\n", ".", " ", ""]
     )
     return splitter.split_documents(docs)
